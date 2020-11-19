e730ed010e3c30175d92dfb5a3a894544a770b44
Matthheuz <54036392+Matthheuz@users.noreply.github.com>
2020-11-19 19:20:46 -0300

    Matthheuz
    Casconha

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,16 @@
 
 
 export const environment = {
-  production: true
+  production: true, 
+  firebase : {
+    apiKey: "AIzaSyBP82qhUXtDTzoIMiHROdvPRj3fgVGl50o",
+    authDomain: "projeto6-42674.firebaseapp.com",
+    databaseURL: "https://projeto6-42674.firebaseio.com",
+    projectId: "projeto6-42674",
+    storageBucket: "projeto6-42674.appspot.com",
+    messagingSenderId: "393827080858",
+    appId: "1:393827080858:web:5794d6b0b04a829f835d23"
+  }
 };
 
 
