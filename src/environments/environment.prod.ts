79b6e08fd7d6752e37568d79a81e06205c420408
zxcvdav <davidcarvalho563@gmail.com>
2021-02-02 18:25:54 -0300

    alterações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAmnXMdGszYFYWpZt1kggVXH2Cxt2zUw0I",
+    authDomain: "projeto6-8d7ec.firebaseapp.com",
+    databaseURL: "https://projeto6-8d7ec.firebaseio.com",
+    projectId: "projeto6-8d7ec",
+    storageBucket: "projeto6-8d7ec.appspot.com",
+    messagingSenderId: "136931920902",
+    appId: "1:136931920902:web:0046c414680a8f39e84787"
+  }
 };
